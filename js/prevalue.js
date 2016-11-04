aea47507e61460e937dec7c3eca0e16a798a0f1c
Filipe Guarnieri <filipe@ibaro.com.br>
2016-11-04 16:47:08 -0200

    ADD: extend "value", "end"

--- js/prevalue.js
+++ js/prevalue.js
@@ -1,13 +1,36 @@
-;(function($){
-    $.fn.prevalue = function(prevalue){
-        $(this).each(function(e, f){
-            var valor = prevalue||f.getAttribute('data-prevalue')
-            , id = f.id
-            , style = f.classList
-            ;
-            console.log(style.value);
-            $(f).wrap('<span class="'+style+'">').removeAttr('class').addClass('prevalue-input').before('<label for="'+id+'" class="prevalue-value">'+valor+'</label>').parent().addClass('prevalue-wrap');
-            $(f).css({paddingLeft: (f.previousElementSibling.clientWidth-1)+'px'});
-        });
-    }
-})(jQuery);
+$.fn.prevalue = function(option){
+    var item = $.extend({
+          value: false
+        , end: false
+    }, option);
+    this.each(function(e, f){
+        var valor = (typeof option==='string') ? option : (item.value||f.getAttribute('data-prevalue')) 
+        , id = f.id
+        ;
+        if(id.trim().length===0){
+            id = 'fake_id_'+Date.now();
+            f.id = id;
+        }
+        $(f).wrap('<span class="'+f.classList+'">').removeAttr('class data-prevalue').addClass('prevalue-input').before('<label for="'+id+'" class="prevalue-value">'+valor+'</label>').parent().addClass('prevalue-wrap');
+        $(f).css({paddingLeft: (f.previousElementSibling.clientWidth)+'px'});
+        if(item.end){
+            var label = $(f.previousElementSibling);
+            $(f).css({paddingLeft: 0}).addClass('prevalue-hidden').removeClass('prevalue-input');
+            label.before('<div class="prevalue-input prevalue-end-input" contenteditable="true" />').parent().addClass('prevalue-end');
+            label.prev().on('input', function(g){
+                $(f).val(this.innerHTML+item.value)
+            });
+            $(f).on('focus', function(g){
+                $(this).parent().children('.prevalue-input').trigger('focus');
+            })
+        }
+    });
+};
+$(function(){
+    document.querySelectorAll('[data-prevalue]').forEach(function(e, f){
+        $(e).prevalue({value: e.dataset.prevalue});
+    });
+    document.querySelectorAll('[data-prevalue-end]').forEach(function(e, f){
+        $(e).prevalue({value: e.dataset.prevalueEnd, end: true});
+    });
+})
