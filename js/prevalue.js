32066d20dab5651eb7ffe037c851d8de6af7022f
Filipe Guarnieri <filipe@ibaro.com.br>
2017-01-09 22:45:57 -0200

    FIX: input has hidden

--- js/prevalue.js
+++ js/prevalue.js
@@ -3,34 +3,36 @@ $.fn.prevalue = function(option){
           value: false
         , end: false
     }, option);
+    if(this.is('[this-status="on"]'))return;
     this.each(function(e, f){
-        var valor = (typeof option==='string') ? option : (item.value||f.getAttribute('data-prevalue')) 
-        , id = f.id
+        var t = $(f)
+        , valor = (typeof option==='string') ? option : (item.value||t.attr('data-prevalue'))
+        , id = t.attr('id') ? t.attr('id') : 'fake_id_'+Date.now()
+        , classe = f.classList.value
+        , form = t.closest('form')
+        , divInput = $('<span class="prevalue-input prevalue-content" contenteditable="true" />')
+        , wrapInput = $('<span class="prevalue-wrap '+classe+'">')
+        , textPrevalue = $('<label for="'+id+'" class="prevalue-value">'+valor+'</label>');
         ;
-        if(id.trim().length===0){
-            id = 'fake_id_'+Date.now();
-            f.id = id;
-        }
-        $(f).wrap('<span class="'+f.classList+'">').removeAttr('class data-prevalue').addClass('prevalue-input').before('<label for="'+id+'" class="prevalue-value">'+valor+'</label>').parent().addClass('prevalue-wrap');
-        $(f).css({paddingLeft: (f.previousElementSibling.clientWidth)+'px'});
+        t.attr('this-status', 'on').wrap(wrapInput).attr('id',id).addClass('prevalue-hidden').removeClass(classe).before(divInput).after(textPrevalue);
+        divInput.on('keyup', function(e){
+            t.val(this.textContent);
+        });
+        t.on('focus', function(g){
+            $(this).parent().children('.prevalue-content').trigger('focus');
+        });
         if(item.end){
-            var label = $(f.previousElementSibling);
-            $(f).css({paddingLeft: 0}).addClass('prevalue-hidden').removeClass('prevalue-input');
-            label.before('<div class="prevalue-input prevalue-end-input" contenteditable="true" />').parent().addClass('prevalue-end');
-            label.prev().on('input', function(g){
-                $(f).val(this.innerHTML+item.value)
-            });
-            $(f).on('focus', function(g){
-                $(this).parent().children('.prevalue-input').trigger('focus');
-            })
+            divInput.parent().addClass('prevalue-input-end');
+        } else {
+            divInput.css({paddingLeft: (f.nextElementSibling.clientWidth)+'px'});
         }
     });
 };
 $(function(){
-    document.querySelectorAll('[data-prevalue]').forEach(function(e, f){
-        $(e).prevalue({value: e.dataset.prevalue});
+    $('[data-prevalue]').each(function(e, f){
+        $(f).prevalue({value: f.dataset.prevalue});
     });
-    document.querySelectorAll('[data-prevalue-end]').forEach(function(e, f){
-        $(e).prevalue({value: e.dataset.prevalueEnd, end: true});
+    $('[data-prevalue-end]').each(function(e, f){
+        $(f).prevalue({value: f.dataset.prevalueEnd, end: true});
     });
 })
